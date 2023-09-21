e60c1516fc49cb2dc39a1d7b847acf343fb3a832
Tariq Khan <71902492+tariq-k-dev@users.noreply.github.com>
2023-09-21 07:05:44 -0700

    Add docstrings, remove unused code

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-# from string import ascii_lowercase
-import json
+from os import name, system
 
 BOOK_PATH = "books/frankenstein.txt"
 
 
-def word_count(book):
+def clear() -> None:
+    """Clears console at start of program."""
+    if name == "nt":
+        system("cls")
+    else:
+        system("clear")
+
+
+def word_count(book) -> int:
+    """Takes a book's text as input\nReturns total number of words."""
     return len(book.split())
 
 
-def character_count(book):
+def character_count(book: str) -> dict:
+    """
+    Takes a book's text as input.\n
+    Returns a dictionary of letters as keys with their total counts as values.
+    """
+
     letter_count = {}
 
     for char in book:
-        # if char.lower() not in ascii_lowercase:
-        #     continue
-
         if not char.isalpha():
             continue
 
@@ -32,18 +42,19 @@ def character_count(book):
     return sorted_letters
 
 
-def book_report(book_content):
+def book_report(book_content: str) -> None:
+    """Prints a summary of a books word and letter count inb the console"""
+
+    clear()
     print(f"--- Begin report of {BOOK_PATH} ---\n")
     print(f"{word_count(book_content)} words found in the document\n")
-
-    # print(f"Frankenstein Book letter count: \n{json.dumps(character_count(book_content), indent=4)}")
 
     for letter, count in character_count(book_content).items():
         print(f"The '{letter}' character was found {count} times")
     print("\n--- End report ---\n")
 
 
+# Get a book's text content, and generate a report
 with open(BOOK_PATH) as f:
     file_contents = f.read()
-
     book_report(file_contents)
